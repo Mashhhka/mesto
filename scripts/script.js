e828d02df6e9be0d44288afc73140ef34daf9a85
Masha <dru4ka@yandex.ru>
2022-05-17 12:57:31 +0300

    valid

--- scripts/script.js
+++ scripts/script.js
@@ -12,13 +12,15 @@ const nameInput = document.querySelector(".form__input_type_user-name");
 const jobInput = document.querySelector(".form__input_type_user-job");
 const linkCardInput = document.querySelector(".form__input_type_link-card");
 const titleCardInput = document.querySelector(".form__input_type_title-card");
-const formButton = document.querySelector(".form__button");
+const buttonElement = document.querySelector(".form__button");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const CardList = document.querySelector('.card-list');
 const CardTemplate = document.querySelector('.elements-template');
 const PopupImage = document.querySelector('.popup__image');
 const PopupImageTitle = document.querySelector('.popup__image-title');
+const formInput = document.querySelector('.form__input');
+
 
 // Шаблоны
 
@@ -110,3 +112,63 @@ formCreateCard.addEventListener("submit", handleSubmitAddCardForm);
 
 
 
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.form__button');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+  return !inputElement.validity.valid;
+});
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("button_inactive");
+  } else {
+    buttonElement.classList.remove('button_inactive');
+  }
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+    });
+    setEventListeners(formElement)
+    });
+  };
+  enableValidation();
